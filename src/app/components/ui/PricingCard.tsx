96fd2422ac6f2c1aae74cd19b07686195d98af9f
Cristina <avicon95@protonmail.com>
2024-12-09 18:09:10 -0500

    chore: Pricing tag changes

--- src/app/components/ui/PricingCard.tsx
+++ src/app/components/ui/PricingCard.tsx
@@ -14,7 +14,7 @@ const PricingCard: React.FC<PricingCardProps> = ({title, type, description, butt
         <div className={"bg-tertiary rounded-3xl py-10 flex flex-col justify-center items-center w-full max-w-[480px]"}>
             <h2 className={"text-primary mb-3"}>{type}</h2>
             <h2 className={"mb-4"}>{title}</h2>
-            <div className={"h-[180px] sm:w-[440px] mb-5 text-center"}>
+            <div className={"h-[180px] sm:w-[400px] mb-5 text-center"}>
                 {description}
             </div>
             <Button className={"w-44"} text={buttonText} style={"PRIMARY"} onClick={buttonAction}/>
